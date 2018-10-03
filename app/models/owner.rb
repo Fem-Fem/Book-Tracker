1abfb1f3d8b199ea82679417c500cb06004d68bd
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-03 23:50:04 +0000

    Working on validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,9 @@ class Owner < ActiveRecord::Base
   has_many :books
   has_secure_password
   validates_presence_of :name, :username, :password
+  validates :username, uniqueness: true
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 1 }
+  validates :username, length: { minimum: 1 }
+  validates :password, length: { minimum: 1 }
 end
