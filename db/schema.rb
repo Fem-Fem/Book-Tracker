b8e06c13851321a7805c99a91c02842b82719ab0
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-31 15:04:56 +0000

    Removing backpack functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
-
-  create_table "backpacks", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
