c09ea9caf0767f094257248c971eaec51060971b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-02 02:03:46 +0000

    Remove superfluous bindings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
 femi = Owner.create(username: "Obafemi", password: "password", name: "femi")
 harry_potter = Book.create(title: "Harry Potter", author: "JK Rowling", genre: "Fantasy", summary: "Magic Stuff")
-binding.pry
 harry_potter.owner_id = femi.id
-binding.pry
 harry_potter.save
 percy_jackson = Book.create(title: "Percy Jackson", author: "Rick Riordan", genre: "Fantasy", summary: "Greek Stuff")
 percy_jackson.owner_id = femi.id
 percy_jackson.save
-binding.pry
 
 remi = Owner.create(username: "Obaremi", password: "password", name: "remi")
 harry_potter_2 = Book.create(title: "Harry Potter_2", author: "JK Rowling", genre: "Fantasy", summary: "Magic Stuff")
